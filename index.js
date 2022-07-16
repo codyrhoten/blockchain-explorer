49e6fd637aadc01ce08e30b7e870f76df3e99dbe
codyrhoten <74472753+codyrhoten@users.noreply.github.com>
2022-07-16 12:17:07 -0400

    add packages to use for server

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // use?
+const cors = require('cors');
+// const axios = require('axios');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
