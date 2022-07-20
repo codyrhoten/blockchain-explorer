2e4ecda0076893ee9ed0093d2eb97c2c7e4a03a4
codyrhoten <74472753+codyrhoten@users.noreply.github.com>
2022-07-19 21:39:18 -0400

    create transaction class

--- blockchain.js
+++ blockchain.js
@@ -1,3 +1,2 @@
-const sha256 = require('js-sha256');
 const initialUrl = process.env.NODE_1; // write other nodes into blockchain
 import Transaction from './transaction';
--- transaction.js
+++ transaction.js
@@ -1,7 +1,30 @@
-module.exports = class Transaction {
-    constructor(
+const hash = require('hash.js');
 
-    ) {
+module.exports = class Transaction {
+    constructor(amount, hash, recipient, sender, timestamp) {
+        this.amount = amount;
+        this.hash = hash;
+        this.recipient = recipient;
+        this.sender = sender;
+        this.timestamp = timestamp;
         
+        if (this.hash === undefined) this.calculateHash();
     }
+
+    calculateHash() {
+        let txData = {
+            'amount': this.amount,
+            'recipient': this.recipient,
+            'sender': this.sender,
+            'timestamp': this.timestamp
+        }
+
+        let txDataJSON = JSON.stringify(txData);
+        this.hash = sha256(txDataJSON);
+    }
+    // using the hash.js library for sha256 hashing
+    sha256(data) {
+        return hash.sha256().update(data).digest('hex');
+    }
+
 }
