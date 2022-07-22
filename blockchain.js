27204a1f4c4c443a3ea1ea9364ed41d80873d8a5
codyrhoten <74472753+codyrhoten@users.noreply.github.com>
2022-07-22 00:00:12 -0400

    start blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -1,3 +1,13 @@
 const initialUrl = process.env.NODE_1; // write other nodes into blockchain
 import Transaction from './transaction';
 import Block from './block';
+// import validation file
+
+class Blockchain {
+    constructor() {
+        this.chain = [/* genesis block */];
+        this.difficulty = '';
+        this.nodes = [];
+        this.pending_transactions = [];
+    }
+}
